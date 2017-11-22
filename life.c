19a15f8bf9dde9dff12f97f5fd9eeba2189b2185
Huang  Haoen <haoen95@gmail.com>
2017-11-22 17:01:34 -0500

    Removed mod statements from the inner loop, runtime 120s -> 57s

--- life.c
+++ life.c
@@ -5,9 +5,30 @@
 #include "life.h"
 #include "util.h"
 
+#define SWAP_BOARDS( b1, b2 )  do { \
+  char* temp = b1; \
+  b1 = b2; \
+  b2 = temp; \
+} while(0)
+
+#define BOARD( __board, __i, __j )  (__board[(__i) + LDA*(__j)])
+/*
+#define BOARD_9(__board, __i, __j)  BOARD (inboard, inorth, jwest) + 
+                    BOARD (inboard, inorth, j) + 
+                    BOARD (inboard, inorth, jeast) + 
+                    BOARD (inboard, i, jwest) +
+                    BOARD (inboard, i, jeast) + 
+                    BOARD (inboard, isouth, jwest) +
+                    BOARD (inboard, isouth, j) + 
+                    BOARD (inboard, isouth, jeast);
+*/
+
 /*****************************************************************************
  * Helper function definitions
  ****************************************************************************/
+
+char* game_of_life_parallel (char* outboard, char* inboard, const int nrows,
+        const int ncols, const int gens_max);
 
 /*****************************************************************************
  * Game of life implementation
@@ -19,5 +40,83 @@ game_of_life (char* outboard,
 	      const int ncols,
 	      const int gens_max)
 {
-  return sequential_game_of_life (outboard, inboard, nrows, ncols, gens_max);
+  return game_of_life_parallel (outboard, inboard, nrows, ncols, gens_max);
+}
+
+char*
+game_of_life_parallel (char* outboard, 
+        char* inboard,
+        const int nrows,
+        const int ncols,
+        const int gens_max)
+{
+    /* HINT: in the parallel decomposition, LDA may not be equal to
+       nrows! */
+    const int LDA = nrows;
+    int curgen, i, j;
+    int inorth;
+    int isouth;
+    int jwest;
+    int jeast;
+    for (curgen = 0; curgen < gens_max; curgen++)
+    {
+        /* HINT: you'll be parallelizing these loop(s) by doing a
+           geometric decomposition of the output */
+        for (i = 0; i < nrows; i++) {
+			inorth = mod (i-1, nrows);
+    		isouth = mod (i+1, nrows);
+			jwest = ncols - 1;
+			jeast = 1;
+			j = 0;
+			char neighbor_count = 
+                    BOARD (inboard, inorth, jwest) + 
+                    BOARD (inboard, inorth, j) + 
+                    BOARD (inboard, inorth, jeast) + 
+                    BOARD (inboard, i, jwest) +
+                    BOARD (inboard, i, jeast) + 
+                    BOARD (inboard, isouth, jwest) +
+                    BOARD (inboard, isouth, j) + 
+                    BOARD (inboard, isouth, jeast);
+			BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
+			
+            for (j = 1; j < ncols; j++) {
+    			jwest = j - 1;
+    			jeast = j + 1;
+                neighbor_count = 
+                    BOARD (inboard, inorth, jwest) + 
+                    BOARD (inboard, inorth, j) + 
+                    BOARD (inboard, inorth, jeast) + 
+                    BOARD (inboard, i, jwest) +
+                    BOARD (inboard, i, jeast) + 
+                    BOARD (inboard, isouth, jwest) +
+                    BOARD (inboard, isouth, j) + 
+                    BOARD (inboard, isouth, jeast);
+
+                BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
+            }
+			jeast = 0;
+			jwest = ncols - 2;
+			j = ncols - 1;
+            neighbor_count = 
+                    BOARD (inboard, inorth, jwest) + 
+                    BOARD (inboard, inorth, j) + 
+                    BOARD (inboard, inorth, jeast) + 
+                    BOARD (inboard, i, jwest) +
+                    BOARD (inboard, i, jeast) + 
+                    BOARD (inboard, isouth, jwest) +
+                    BOARD (inboard, isouth, j) + 
+                    BOARD (inboard, isouth, jeast);
+
+            BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
+        }
+        SWAP_BOARDS( outboard, inboard );
+
+    }
+    /* 
+     * We return the output board, so that we know which one contains
+     * the final result (because we've been swapping boards around).
+     * Just be careful when you free() the two boards, so that you don't
+     * free the same one twice!!! 
+     */
+    return inboard;
 }
