dc85d17ee71cacb29c4b26964d9b0d19a3de48ce
Huang  Haoen <haoen95@gmail.com>
2017-11-23 22:18:08 -0500

    Created a modify_columns function in preparation for multithreading

--- life.c
+++ life.c
@@ -12,6 +12,7 @@
 } while(0)
 
 #define BOARD( __board, __i, __j )  (__board[(__i) + LDA*(__j)])
+#define NUM_THREADS 1
 
 /*****************************************************************************
  * Helper function definitions
@@ -33,48 +34,55 @@ game_of_life (char* outboard,
   return game_of_life_parallel (outboard, inboard, nrows, ncols, gens_max);
 }
 
+inline void modify_columns(int start, int end, char* outboard, char* inboard,
+                           const int nrows, const int ncols) {
+    int i, j;
+    int inorth, isouth, jwest, jeast;
+    char neighbor_count;
+    const int LDA = ncols;
+    for (j = start; j < end; j++) {
+        jwest = j-1;
+        jeast = j+1;
+        if (j == 0) {
+            jwest = ncols - 1;
+        } else if (j == ncols - 1) {
+            jeast = 0;
+        }
+
+        for (i = 0; i < nrows; i++) {
+            inorth = i-1;
+            isouth = i+1;
+            if (i == 0) {
+                inorth = nrows - 1;
+            } else if (i == nrows - 1) {
+                isouth = 0;
+            }
+            neighbor_count = 
+                BOARD (inboard, inorth, jwest) + 
+                BOARD (inboard, inorth, j) + 
+                BOARD (inboard, inorth, jeast) + 
+                BOARD (inboard, i, jwest) +
+                BOARD (inboard, i, jeast) + 
+                BOARD (inboard, isouth, jwest) +
+                BOARD (inboard, isouth, j) + 
+                BOARD (inboard, isouth, jeast);
+
+            BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
+
+        }
+    }
+}
+
 char* game_of_life_parallel (char* outboard, char* inboard, const int nrows, 
                              const int ncols, const int gens_max) {
     /* HINT: in the parallel decomposition, LDA may not be equal to
        nrows! */
-    const int LDA = ncols;
-    int curgen, i, j;
-    int inorth, isouth, jwest, jeast;
-    char neighbor_count;
+    int curgen;
     for (curgen = 0; curgen < gens_max; curgen++) {
         /* HINT: you'll be parallelizing these loop(s) by doing a
            geometric decomposition of the output */
-        for (j = 0; j < ncols; j++) {
-            jwest = j-1;
-            jeast = j+1;
-            if (j == 0) {
-                jwest = ncols - 1;
-            } else if (j == ncols - 1) {
-                jeast = 0;
-            }
+        modify_columns(0, ncols, outboard, inboard, nrows, ncols);
 
-            for (i = 0; i < nrows; i++) {
-                inorth = i-1;
-                isouth = i+1;
-                if (i == 0) {
-                    inorth = nrows - 1;
-                } else if (i == nrows - 1) {
-                    isouth = ncols - 1;
-                }
-                neighbor_count = 
-                    BOARD (inboard, inorth, jwest) + 
-                    BOARD (inboard, inorth, j) + 
-                    BOARD (inboard, inorth, jeast) + 
-                    BOARD (inboard, i, jwest) +
-                    BOARD (inboard, i, jeast) + 
-                    BOARD (inboard, isouth, jwest) +
-                    BOARD (inboard, isouth, j) + 
-                    BOARD (inboard, isouth, jeast);
-
-                BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
-
-            }
-        }
         SWAP_BOARDS( outboard, inboard );
 
     }
