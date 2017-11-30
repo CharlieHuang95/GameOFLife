d537a40b9cc9be61cc6612c36e203ff5bbbd6516
michelleleiii <michellerp1013@gmail.com>
2017-11-30 15:35:47 -0500

    Added descriptions for life.c and each function in it. Also make the global barrier variable local

--- life.c
+++ life.c
@@ -1,6 +1,21 @@
 /*****************************************************************************
  * life.c
- * Parallelized and optimized implementation of the game of life resides here
+ * 1)We parallelized GoL with pthreads and twp syncrhomization primitives 
+ * supported by the pthread libirary.
+ *      -1.1) pthread create and join. pthread join is called at the end of 
+ *            the program, right before we return the output board
+ *      -1.2) pthread barriers. At the end of each generation, each thread is
+ *            blocked until all the threads finish that generation
+ * 2)Workload is evenly distributed among threads. Wordload per thread is
+ *   parametrized by the NUM_THREADS macro
+ * 3)Did some hand code optimizations
+ *      -3.1) Replaced the modulo instructions in the original sequential 
+ *            implmentation.
+ *      -3.2) Did loop-unrolling. Index increases by 2. Reduced overhead in the
+ *            for loop.
+ *      -3.3) Reduced memory access from 8 accesses/element to 
+ *            6 accesses/2 elements = 3/elements.
+ *            For details, look at modify_columns implementation.
  ****************************************************************************/
 #include "life.h"
 #include "util.h"
@@ -18,7 +33,9 @@
 #define DEBUG 0
 
 /*****************************************************************************
- * Helper function definitions
+ * Function Prototype: game_of_life_parallel 
+ * game_of_life_parallel is called by game_of_life,
+ * is a parallelized implementation.
  ****************************************************************************/
 
 char* game_of_life_parallel (char* outboard, char* inboard, const int nrows,
@@ -45,10 +62,20 @@ struct ThreadParameters {
     int nrows;
     int ncols;
     int num_iterations;
+    pthread_barrier_t* barrier;
 };
 
-pthread_barrier_t iteration_barrier;
-
+/*****************************************************************************
+ * Function definition: modify_columns
+ * modify_columns looks at elements by rows then columns. We have three
+ * optimizations.
+ * 1) Reduction in strength: It removed the modulo operation, which is in the 
+ * original sequential implementation, by separating the two problematic rows 
+ * (the first and last rows) from the inner loop.
+ * 2) Loop-unrolling: index increments by 2. Reduced for loop overhead
+ * 3) Reduction in cache accesses: from 8/element to 3/element by passing values
+ * from registers to registers, instead of reading from memory between iterations
+ ****************************************************************************/
 void* modify_columns(void *arguments) {
     struct ThreadParameters* thread_params = arguments;
     int i, j;
@@ -60,6 +87,7 @@ void* modify_columns(void *arguments) {
     char* outboard = thread_params->outboard;
     const int ncols = thread_params->ncols;
     const int nrows = thread_params->nrows;
+    const pthread_barrier_t* barrier = thread_params->barrier;
     for (int iter = 0; iter < thread_params->num_iterations; iter++) {
         for (j = thread_params->start; j < thread_params->end; j++) {
             jwest = j-1;
@@ -134,11 +162,19 @@ void* modify_columns(void *arguments) {
         temp = inboard;
         inboard = outboard;
         outboard = temp;
-        pthread_barrier_wait(&iteration_barrier);
+        pthread_barrier_wait(barrier);
     }
     return NULL;
 }
 
+/*****************************************************************************
+ * Function definition: game_of_life_parallel 
+ * game_of_life_parallel creates NUM_THREAD threads and
+ * give an approximately even amount of workload to each thread. Workload
+ * is divided evenly by columns.
+ * Threads also joined in this function.
+ * game_of_life_parallel returns the final board result to its caller
+ ****************************************************************************/
 char* game_of_life_parallel (char* outboard, char* inboard, const int nrows, 
                              const int ncols, const int gens_max) {
     /* HINT: in the parallel decomposition, LDA may not be equal to
@@ -148,6 +184,7 @@ char* game_of_life_parallel (char* outboard, char* inboard, const int nrows,
     }
     struct ThreadParameters thread_params[NUM_THREADS];
     pthread_t tids[NUM_THREADS];
+    pthread_barrier_t iteration_barrier;
     pthread_barrier_init(&iteration_barrier, NULL, NUM_THREADS);
     for (int i = 0; i < NUM_THREADS; i++) {
         thread_params[i].ncols = ncols;
@@ -157,6 +194,7 @@ char* game_of_life_parallel (char* outboard, char* inboard, const int nrows,
         thread_params[i].inboard = inboard;
         thread_params[i].outboard = outboard;
         thread_params[i].num_iterations = gens_max;
+        thread_params[i].barrier = &iteration_barrier;
         pthread_create(&tids[i], NULL, &modify_columns, &thread_params[i]);
     }
     for (int i = 0; i < NUM_THREADS; ++i) {
