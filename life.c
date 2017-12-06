7ea7bb858150bbdf82c3f827d0d1cafb4661dc2f
michelleleiii <michellerp1013@gmail.com>
2017-12-06 14:39:19 -0500

    Commented version with 8 threads

--- life.c
+++ life.c
@@ -1,12 +1,26 @@
 /*****************************************************************************
  * life.c
- * Parallelized and optimized implementation of the game of life resides here
+ * 1)We parallelized GoL with pthreads and twp syncrhomization primitives 
+ * supported by the pthread libirary.
+ *      -1.1) pthread create and join. pthread join is called at the end of 
+ *            the program, right before we return the output board
+ *      -1.2) pthread barriers. At the end of each generation, each thread is
+ *            blocked until all the threads finish that generation
+ * 2)Workload is evenly distributed among threads. Wordload per thread is
+ *   parametrized by the NUM_THREADS macro
+ * 3)Did some hand code optimizations inside the "modify_columns" function:
+ *      -3.1) created local variables to store the passed-in parameters.
+ *      -3.2) changed alive_p function by modifying the logic by writing out 
+                a Karnaugh Map to determine the least amount of logic required.   
+ *      -3.3) Replaced the modulo instructions in the original sequential 
+ *            implmentation.
  ****************************************************************************/
 #include "life.h"
 #include "util.h"
 #include "pthread.h"
 #include <stdlib.h>
 
+#define BOARD( __board, __i, __j )  (__board[(__i) + LDA*(__j)])
 #define SWAP_BOARDS( b1, b2 )  do { \
   char* temp = b1; \
   b1 = b2; \
@@ -17,7 +31,9 @@
 #define DEBUG 0
 
 /*****************************************************************************
- * Helper function definitions
+ * Function Prototype: game_of_life_parallel 
+ * game_of_life_parallel is called by game_of_life,
+ * is a parallelized implementation.
  ****************************************************************************/
 
 char* game_of_life_parallel (char* outboard, char* inboard, const int nrows,
@@ -44,20 +60,26 @@ struct ThreadParameters {
     int nrows;
     int ncols;
     int num_iterations;
+    pthread_barrier_t* barrier;
 };
 
-pthread_barrier_t iteration_barrier;
 
-inline char board_sum(char* board, int i, int jwest, int j, int jeast) {
-    return board[j + i - 1] + board[j + i + 1] +
-           board[jwest + i - 1] + board[jwest + i] + board[jwest + i + 1] +
-           board[jeast + i - 1] + board[jeast + i] + board[jeast + i + 1];
-}
-
+/*****************************************************************************
+ * Function definition: modify_columns
+ * modify_columns looks at elements by rows then columns.
+ * 1. Reduce memory references by creating local variabes for the parameters
+ * pointed by the arguments pointer.
+ * 2. Function inlining and reduction in strength: removed the alive_p function
+ * call by modifying the logic by writing out a Karnaugh Map to determine
+ * the least amount of logic required.
+ * 3. Reduction in strength: It removed the modulo operation, which is in the 
+ * original sequential implementation, by separating the two problematic rows 
+ * (the first and last rows) from the inner loop.
+ ****************************************************************************/
 void* modify_columns(void *arguments) {
     struct ThreadParameters* thread_params = arguments;
     int i, j;
-    int inorth, isouth, jwest, jeast;
+    int jwest, jeast;
     int neighbor_count;
     int LDA = thread_params->ncols;
     char* temp;
@@ -67,6 +89,7 @@ void* modify_columns(void *arguments) {
     const int nrows = thread_params->nrows;
     const int start = thread_params->start;
     const int end = thread_params->end;
+    pthread_barrier_t* barrier = thread_params->barrier;
     for (int iter = 0; iter < thread_params->num_iterations; iter++) {
         for (j = start; j < end; j++) {
             jwest = j-1;
@@ -78,7 +101,6 @@ void* modify_columns(void *arguments) {
                 BOARD (inboard, 0, jwest) + BOARD (inboard, 0, jeast) + BOARD (inboard, 1, jwest) + BOARD (inboard, 1, j) + 
                 BOARD (inboard, 1, jeast);
             BOARD(outboard, 0, j) = (neighbor_count == 3) || (neighbor_count == 2 && BOARD (inboard, 0, j));
-            int offset = jwest * ncols;
             for (i = 1; i < nrows - 1; i++) {
                 neighbor_count = (inboard[jwest*ncols + i - 1] + inboard[jwest*ncols + i] + inboard[jwest*ncols + i + 1]) +
                                    (inboard[j*ncols + i - 1] + inboard[j*ncols + i + 1]) +
@@ -96,16 +118,25 @@ void* modify_columns(void *arguments) {
         temp = inboard;
         inboard = outboard;
         outboard = temp;
-        pthread_barrier_wait(&iteration_barrier);
+        pthread_barrier_wait(barrier);
     }
     return NULL;
 }
 
+/*****************************************************************************
+ * Function definition: game_of_life_parallel 
+ * game_of_life_parallel creates NUM_THREAD threads and
+ * give an approximately even amount of workload to each thread. Workload
+ * is divided evenly by columns.
+ * Threads also joined in this function.
+ * game_of_life_parallel returns the final board result to its caller
+ ****************************************************************************/
 char* game_of_life_parallel (char* outboard, char* inboard, const int nrows, 
                              const int ncols, const int gens_max) {
     struct ThreadParameters thread_params[NUM_THREADS];
     pthread_t tids[NUM_THREADS];
-    pthread_barrier_init(&iteration_barrier, NULL, NUM_THREADS);
+    pthread_barrier_t* iteration_barrier = malloc(sizeof(pthread_barrier_t));
+    pthread_barrier_init(iteration_barrier, NULL, NUM_THREADS);
     for (int i = 0; i < NUM_THREADS; i++) {
         thread_params[i].ncols = ncols;
         thread_params[i].nrows = nrows;
@@ -114,6 +145,7 @@ char* game_of_life_parallel (char* outboard, char* inboard, const int nrows,
         thread_params[i].inboard = inboard;
         thread_params[i].outboard = outboard;
         thread_params[i].num_iterations = gens_max;
+        thread_params[i].barrier = iteration_barrier;
         pthread_create(&tids[i], NULL, &modify_columns, &thread_params[i]);
     }
     for (int i = 0; i < NUM_THREADS; ++i) {
